74cdf0eabf702243709427ff9b9e926427280fcf
karl wernsman <wernsmank@gmail.com>
2023-01-27 12:01:34 -0800

    added ganmeMesssage handling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Board from './component/board/Board.js';
+import { useGameContext } from './context/GameContext.js';
 
 function App() {
+  const { gameMessage } = useGameContext();
   return (
     <div>
+      <p>{gameMessage}</p>
       <Board />
     </div>
   );
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -9,7 +9,7 @@ const GameProvider = ({ children }) => {
   const [currentPlayer, setCurrentPlayer] = useState('x'); //'x' or 'o'
   const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']); //showcasing taken and open boxes
   const [active, setActive] = useState(true); //boolean
-  const [gameMessage, setGameMessage] = useState('Your turn, X.'); //'Your turn __', 'You win __' or 'It's a cat's game!'
+  const [gameMessage, setGameMessage] = useState('Your turn, x.'); //'Your turn __', 'You win __' or 'It's a cat's game!'
 
   const handleBoxClick = (index) => {
     if (!active) return;
@@ -21,7 +21,13 @@ const GameProvider = ({ children }) => {
 
     if (currentPlayer === 'x') setCurrentPlayer('o');
     if (currentPlayer === 'o') setCurrentPlayer('x');
+
+    setGameMessage(`Your turn, ${currentPlayer}.`);
   };
+
+  const checkGameState = () => {};
+
+  const winConditions = () => {};
 
   return (
     <GameContext.Provider
